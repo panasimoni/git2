3f64b4da017cdfb0dd648ab919b395d73b7d077f
simon <simonescuela9@gmail.com>
2024-02-28 12:51:12 +0000

    La Clase Persona a sido creada

---
+++ poo1/main.py
@@ -0,0 +1,16 @@
+#1 Que palabra reservada se utiliza en lugar de animal (nombre de la clase) para acceder al atributo patas
+
+#2 Que paalbra reservada hay que utilizar para crear un nuevo objeto
+
+from alumno import alumno
+
+
+
+def main():
+    daniel = alumno("daniel", 4)
+    daniel.imprimir()
+    daniel.promociona()
+
+
+if __name__ == "__main__":
+    main()
---
+++ poo1/persona.py
@@ -0,0 +1,17 @@
+class Persona :
+    def __init__(self,nombre,anios):
+        self.nombre = nombre
+        self.anios = anios
+        
+
+    def imprimir(self):
+        print(self.nombre, "Tiene", self.anios,"años")
+
+        def cumpleanios(self):
+            self.anios += 1 
+            self.imprimir()
+
+    if __name__ == "__main__":
+        obama = Persona("Obama", 62)
+        obama.imprimir()
+        obama.cumpleanios()
