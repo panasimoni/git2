0c44253d611acc8250e540ee4be7b6e6018dc1bd
simon <simonescuela9@gmail.com>
2024-04-12 09:50:21 +0100

    prueba 1

---
+++ poo4/except1.py
@@ -0,0 +1,15 @@
+import random
+
+def intDiv(a:int, b:int)->int:
+    try:
+        return a // b
+    except:
+        print("Realizamos tareas de control de cierre")
+
+
+#si es el flujo principal
+if __name__ == "__main__":
+    for i in range(30):
+        a = random.randint(0,9)
+        b = random.randint(0,9)
+        print("a:",a, "b:",b, intDiv(a,b))
